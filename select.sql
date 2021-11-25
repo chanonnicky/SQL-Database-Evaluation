b0ac21c82aa167f95512dd73e9767fbc9a0d8095
chanonnicky <81746336+chanonnicky@users.noreply.github.com>
2021-11-25 22:44:11 +0700

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,21 @@
+select * from course;
+
+select * from student;
+
+select * from student_reg ;
+
+select * from teacher ;
+
+select * from teacher_reg;
+
+select * from Course_eva;
+
+select * from teacher_eva;
+
+select * from COURSE_REC;
+
+select * from TEACHER_REC;
+
+select * from STUDENT_REC;
+
+select * from COURSE_FOR_REG;
